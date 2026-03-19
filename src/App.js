a9c044b4cb6dd2b4ebf3fa1e31810d6243857fdb
markfelps <marcosfelipeagostinho@gmail.com>
2026-03-19 09:08:24 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ export default function ValorantAuthorizationApp() {
         <select name="monitor" value={form.monitor} onChange={handleChange} required>
           <option value="">Selecione o monitor</option>
           <option value="Marcos">Marcos</option>
-          <<option value="Nathan">Nathan</option>
+          <option value="Nathan">Nathan</option>
           <option value="Outro">Outro</option>
         </select>
         <br /><br />
